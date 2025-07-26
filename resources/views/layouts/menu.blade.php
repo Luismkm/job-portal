d7014115d94b15a76eba748244123e25797f2064
Luis Moraes <luismarcelo13@yahoo.com.br>
2025-07-26 17:54:52 -0300

    feat: initial layouts

---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,17 @@
+<div class="bg-gray-800 h-20 w-full">
+    <div class="flex justify-between items-center h-full mx-[72px]">
+        <div>
+            <a href="#"><img src="{{ asset('/img/logo.png') }}" alt=""></a>
+        </div>
+        <ul class="flex gap-5 text-white">
+            <a href="#"><li class="font-semibold hover:font-semibold">Home</li></a>
+            <a href="#"><li class="font-medium hover:font-semibold">Jobs</li></a>
+            <a href="#"><li class="font-medium hover:font-semibold">About Us</li></a>
+            <a href="#"><li class="font-medium hover:font-semibold">Contact Us</li></a>
+        </ul>
+        <div class="flex text-white gap-4">
+            <button class="hover:font-semibold">Login</button>
+            <button class="w-24 h-10 bg-primary rounded-lg hover:font-semibold hover:transition-colors">Register</button>
+        </div>
+    </div>
+</div>
